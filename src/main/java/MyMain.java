3fe21ef868a8555268922c1fe7642af9f8202dc7
Jonah-Has <23jhasegawa@athenian.org>
2021-01-19 06:59:46 +0000

    wrote code for testMean, testMedian, and testMode.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,20 +3,60 @@ public class MyMain {
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) { 
         // YOUR CODE HERE
-        return -1.0;
+        int cells = 0;
+        int total = 0;
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[0].length; j++){
+                total += mat[i][j];
+                cells += 1;
+            }
+        }
+        return ((double) total / (double) cells);
     }
 
     // Returns the median of the 2D array mat
     public static double median(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        int j = mat.length;
+        int i = mat[0].length;
+        if (j % 2 == 0){
+            return (((mat [j / 2] [0]) + (mat [(j - 1) / 2] [i - 1])) / 2 );
+        }
+        else{
+            return (((mat  [j / 2] [i / 2]) + (mat [j / 2] [(i - 1) / 2])) / 2);
+        }
+        
     }
     
 
     // Returns the mode of the 2D array mat
     public static double mode(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        int most1 = 0;
+        int most2 = 0;
+        double mostnum = 0;
+        double last = -1;
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[0].length; j++){
+                if (mat[i][j] == last){
+                    most1 += 1;
+                    mostnum = last;
+                }
+                else{
+                    if (most1 > most2){
+                        most2 = most1;
+                        most1 = 0;
+                        mostnum = last;
+                    }
+                    else{
+                        most1 = 0;
+                        
+                    }
+                }
+                last = mat [i] [j];
+            }
+        }
+        return mostnum;
+
+
     }
 
 
